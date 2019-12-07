9c1d3039325b3991403a8f878699e9560b5da772
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 00:15:43 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,30 +6,45 @@ array_1 = [
 ]
 
 outer_results = []
+
 row_index = 0
 while row_index < array_1.count do
-  element_index = 0
-  inner_results = "I love "
-  while element_index < array_1[row_index].count do
-    puts inner_results << array_1[row_index].join(' and ') "on my pizza"
-    element_index += 1
-  end
+  inner_results = array_1[row_index]
+  outer_results << "I love #{inner_results[0]} and #{inner_results[1]} on my pizza"
   row_index += 1
 end
+
+outer_results
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
+
+  outer_results = []
+
+  row_index = 0
+  while row_index < array_2.count do
+    inner_results = array_2[row_index]
+    outer_results << (array_2[row_index][0] > array_2[row_index][1] ? array_2[row_index][0] : array_2[row_index][1])
+    row_index += 1
+  end# src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  outer_results# that are in the inner Arrays
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
+
+  total = 0
+
+  row_index = 0
+  while row_index < array_3.count do
+    if (array_3[row_index][0] % 2 == 0) && (array_3[row_index][1] % 2 == 0)
+      total += (array_3[row_index][0] + array_3[row_index][1])
+    end
+
+    row_index += 1
+  end
+
+  total
 end
